3ff8f33ecff7951cee6e1deb5c5149cbd362f36a
piaohaihang <piaohaihang@u51.com>
2019-10-29 18:59:42 +0800

    curd测试

--- src/main/java/com/stu/demo/controller/user/UserController.java
+++ src/main/java/com/stu/demo/controller/user/UserController.java
@@ -6,6 +6,7 @@ import com.stu.demo.dao.repository.UserMapper;
 import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -14,10 +15,15 @@ import java.util.List;
 @Api(value = "用户api", description = "用户相关api")
 @RestController
 @RequestMapping("/user")
+@Scope("prototype")
+
+
 public class UserController {
 
     @Autowired
     private UserMapper userMapper;
+
+
 
 
 
@@ -30,15 +36,15 @@ public class UserController {
     }
 
 
+
     @ApiOperation(value = "id查询用户信息")
     @ApiImplicitParam(name = "id", required = true, value = "用户ID", paramType = "query", dataType = "int")
     @RequestMapping(value = "/userinfo", method = { RequestMethod.GET})
     public List<User> userInfo2(@RequestParam(value = "id") int id){
         User user = userMapper.selectByPrimaryKey(id);
-        List<User> userArrayList = new ArrayList<>();
         List<User> userList = new ArrayList<>();
-        userArrayList.add(user);
-        return userArrayList;
+        userList.add(user);
+        return userList;
     }
 
 
@@ -48,6 +54,22 @@ public class UserController {
     public String addUser(@RequestBody User user){
 //        User user = userMapper.insert(user);
         return user.getUsername();
+    }
+
+    @RequestMapping(value = "update", method = RequestMethod.GET)
+    public boolean updateUser(){
+        User user = new User();
+        user.setId(1);
+        user.setRealname("大大师兄");
+        user.setUsername("大大da师兄111");
+        userMapper.updateByPrimaryKeySelective(user);
+        return false;
+    }
+    @RequestMapping(value = "del", method = RequestMethod.GET)
+    public boolean delUser(){
+
+        userMapper.deleteByPrimaryKey(1);
+        return false;
     }
 
 
